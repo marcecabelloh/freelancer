1a1579db299f71a86db3ecb2ec071380e3ec0104
marcecabelloherrera <marcecabello.h@gmail.com>
2017-06-01 10:43:52 -0400

    correcciones

--- js/app.js
+++ js/app.js
@@ -3,35 +3,37 @@
  */
  
 
-/*MODAL*/
+ /* CREACION DE MODAL NO FUNCIONA*/
 
-(function main(){
-	var boxes = Array.from(document.getElementsByClassName("imagen"));
-	var modal = document.getElementById("box-services-modal");
-	var bodyModal, close, img;
-	boxes.forEach(function(box){
-		box.addEventListener("click", function(){
-			modal.innerHTML = "";
-			bodyModal = document.createElement("div");
-			bodyModal.classList.add("modal-body");
-			bodyModal.innerHTML = box.innerHTML;			
-			modal.appendChild(bodyModal);
-			modal.classList.remove("hide");
-			close = document.createElement("div");
-			close.classList.add("close");
-			img = document.createElement("img");
-			img.setAttribute("src", "http://tojaeurope.com/gallery/Close-icon.png");
-			close.appendChild(img);
-			modal.appendChild(close);
-			close.addEventListener("click",function(){
-				modal.classList.add("hide");
-			});
-		});		
-	});
-})();
+ (function main(){
+ 	var boxes = Array.from(document.getElementsByClassName("imagen"));
+ 	var modal = document.getElementById("box-services-modal");
+ 	var bodyModal, close, img;
+ 	boxes.forEach(function(box){
+ 		box.addEventListener("click", function(){
+ 			modal.innerHTML = "";
+ 			bodyModal = document.createElement("div");
+ 			bodyModal.classList.add("modal-body");
+ 			bodyModal.innerHTML = box.innerHTML;			
+ 			modal.appendChild(bodyModal);
+ 			modal.classList.remove("hide");
+ 			close = document.createElement("div");
+ 			close.classList.add("close");
+ 			img = document.createElement("img");
+ 			img.setAttribute("src", "http://tojaeurope.com/gallery/Close-icon.png");
+ 			close.appendChild(img);
+ 			modal.appendChild(close);
+ 			close.addEventListener("click",function(){
+ 				modal.classList.add("hide");
+ 			});
+ 		});		
+ 	});
+ })();
 
 
-function mostrarData(){
+/* intento validar campos del formulario*/
+
+ function mostrarData(){
 
  	var nombre = document.getElementById("name").value;
  	var email = document.getElementById("email").value;
